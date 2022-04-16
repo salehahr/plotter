069c9f05bd88231256edc0af71cef862dd8d74e2
s <salehahr@users.noreply.github.com>
2022-04-16 15:41:41 +0200

    configs for nodes_nn, selected sweep runs

--- save_run_metrics.py
+++ save_run_metrics.py
@@ -2,16 +2,25 @@ import wandb
 
 from tools import data, get_config
 
-config = get_config("configs/edge_nn_baseline.yaml")
+config = get_config("configs/nodes_nn.yaml")
 api = wandb.Api()
 
-metrics = ["loss", "accuracy", "precision", "recall"]
+# metrics = ["loss", "accuracy", "precision", "recall"]
+metrics = [
+    "loss",
+    "node_pos_loss",
+    "degrees_loss",
+    "node_types_loss",
+    "node_pos_accuracy",
+    "degrees_accuracy",
+    "node_types_accuracy",
+]
 
 if __name__ == "__main__":
     if config.multiple_runs:
         runs = config.get_runs(api, all=False)
         for run in runs:
-            filename = config.gen_filepath(f"skip_{run.id}")
+            filename = config.gen_filepath(f"{run.id}")
             data.save_metrics(run, metrics, filename)
     else:
         run = config.get_run(api)
